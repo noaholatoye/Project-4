252c7b67ea3b0dbbca5cb1fbc5b0cd47c755b5b9
Noah Olatoye <noaholatoye@gmail.com>
2018-08-10 22:43:05 +0100

    Search Box Update

--- js/app.js
+++ js/app.js
@@ -6,23 +6,22 @@ lightbox.option({
   })
 
 
-  // Declare variables
 var myInput, filter, img, alt, a;
 input = document.getElementById('myInput');
-filter = input.value.toUpperCase();
 image = document.getElementsByTagName('img');
 alt = document.querySelectorAll('[alt]');
+a = document.getElementsByTagName('a.attributes');
 
-console.log(image);
+console.log(a);
 
 
 // Loop through all list items, and hide those who don't match the search query
 input.addEventListener('input', () => {
 
-    for (i = 0; i < image.length; i++) {
-        if(image[i].alt.toUpperCase() === input.value.toUpperCase()){
-            image[i].style.display = "block";
-            console.log("Shown -->", image[i])
+    for (i = 0; i < a.length; i++) {
+        if(title[i].includes(input.value)){
+            console.log(image);
+            a[i].style.display = "block";
         } else if(input.value == ""){
             for (i = 0; i < image.length; i++) {
                 image[i].style.display = "block";
@@ -32,3 +31,35 @@ input.addEventListener('input', () => {
             console.log("Hidden -->", image[i])
         }
 }});
+
+
+
+
+
+  // Declare variables
+// var myInput, filter, img, alt, a;
+// input = document.getElementById('myInput');
+// image = document.getElementsByTagName('img');
+// alt = document.querySelectorAll('[alt]');
+
+// console.log(image);
+
+
+// // Loop through all list items, and hide those who don't match the search query
+// input.addEventListener('input', () => {
+
+//     for (i = 0; i < image.length; i++) {
+//         if(image[i].alt.toUpperCase() === input.value.toUpperCase()){
+//             image[i].style.display = "block";
+//             console.log("Shown -->", image[i])
+//         } else if(input.value == ""){
+//             for (i = 0; i < image.length; i++) {
+//                 image[i].style.display = "block";
+//             }
+//         } else{
+//             image[i].style.display = "none";
+//             console.log("Hidden -->", image[i])
+//         }
+// }});
+
+
