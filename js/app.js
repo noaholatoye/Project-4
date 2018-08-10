28cefc7fbc7f3784d4b7ccbef6ab773862ab7ecb
Noah Olatoye <noaholatoye@gmail.com>
2018-08-10 16:52:45 +0100

    Search Box Update

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ lightbox.option({
     'resizeDuration': 200,
     'wrapAround': true,
     'showImageNumberLabel': false,
-<<<<<<< HEAD
     'maxWidth': 700,
   })
 
@@ -33,31 +32,3 @@ input.addEventListener('input', () => {
             console.log("Hidden -->", image[i])
         }
 }});
-
-
-
-=======
-    'maxWidth': 1000,
-  })
-
-
-
-  document.addEventListener('click', function myFunction() {
-    // Declare variables
-            var myInput, filter, img, alt, a;
-            input = document.getElementById('myInput');
-            filter = input.value.toUpperCase();
-            alt = document.getElementByTagName("alt");
-            li = document.getElementsByTagName('a');
-        
-            // Loop through all list items, and hide those who don't match the search query
-            for (i = 0; i < a.length; i++) {
-                a = li[i].getElementsByTagName("li")[0];
-                if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                    a[i].style.display = "";
-                } else {
-                    a[i].style.display = "none";
-                }
-            }
-        })
->>>>>>> 051a77e6b75083d8ea3ef2d28844cb1baf6f8ef3
