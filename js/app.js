b2aac56a89b7b642e3acc02bd06c101225146dd4
Noah Olatoye <noaholatoye@gmail.com>
2018-08-10 16:45:30 +0100

    Search Box
    Include search box

--- js/app.js
+++ js/app.js
@@ -1,51 +1,37 @@
-
-// Call the plugin
-    lightbox.option({
-      'resizeDuration': 700,
-      'wrapAround': true,
-      'showImageNumberLabel': false,
-      'alwaysShowNavOnTouchDevices': true,
-      'maxWidth': 200,
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,
+    'showImageNumberLabel': false,
+    'maxWidth': 700,
+  })
 
 
-    });
+  // Declare variables
+var myInput, filter, img, alt, a;
+input = document.getElementById('myInput');
+filter = input.value.toUpperCase();
+image = document.getElementsByTagName('img');
+alt = document.querySelectorAll('[alt]');
+
+console.log(image);
 
 
+// Loop through all list items, and hide those who don't match the search query
+input.addEventListener('input', () => {
 
-// Add caption to image 
-
-var image = document.getElementsByTagName('img')
-
-image.addEventListener(‘click’, function(){
-
-  const atr = $(‘img’);
-  let div = $(‘<div>’).addClass(‘desc’);
-  $(‘.gallery’).append(div);
-  const $desc = $(‘.desc’);
-
-  for (var i = 0; i < atr.length; i++) {
-      let getAttr = atr[i].getAttribute(‘atr’);
-      $($desc[i]).html(getAttr);
-  }
-});
-
-
-
-document.addEventListener('click', function myFunction() {
-//     // Declare variables
-    var myInput, filter, img, alt, a;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("alt");
-    li = ul.getElementsByTagName('a');
-
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < a.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            a[i].style.display = "";
-        } else {
-            a[i].style.display = "none";
+    for (i = 0; i < image.length; i++) {
+        if(image[i].alt.toUpperCase() === input.value.toUpperCase()){
+            image[i].style.display = "block";
+            console.log("Shown -->", image[i])
+        } else if(input.value == ""){
+            for (i = 0; i < image.length; i++) {
+                image[i].style.display = "block";
+            }
+        } else{
+            image[i].style.display = "none";
+            console.log("Hidden -->", image[i])
         }
-    }
-});
+}});
+
+
+
